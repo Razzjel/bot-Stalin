e7fa86ef8163083de4b59204dcc14c573607ff6d
Razzjel <astonodane@gmail.com>
2017-11-11 15:35:14 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 'Relchak to') {
+    	message.reply('szmata');
   	}
 });
 
